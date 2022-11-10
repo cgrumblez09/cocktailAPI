7937a758b3c9bba6c7b089cafd53368f8d5edec0
Christian Guizar <guizar_chris@yahoo.com>
2022-11-09 21:22:21 -0600

    Working on side nav bar

--- js/main.js
+++ js/main.js
@@ -237,3 +237,34 @@ function prevCocktail(){
     console.log(`error ${err}`)
     });
 }
+
+console.clear();
+
+const nav = document.getElementById('side-nav');
+const showNavBtn = document.getElementById('show-nav');
+const container = document.getElementById('container');
+const navWidth = 15; // rems
+const navGutter = 1;
+
+nav.addEventListener('click', (event) => {
+	if(event.target.classList.contains('sub-menu-link')){
+    event.target.classList.toggle('active');
+  	const subMenu = event.target.nextElementSibling;
+    subMenu.classList.toggle('active');
+  }
+});
+
+showNavBtn.addEventListener('click', (event) => {
+  if (nav.style.left !== '0px') {
+    showNavBtn.classList.toggle('open');
+    nav.classList.toggle('open');
+  	container.classList.toggle('nav-open');
+    document.body.style.overflow = 'hidden';
+  } else {
+    showNavBtn.classList.toggle('open');
+    nav.classList.toggle('open');
+  	container.classList.toggle('nav-open');
+    document.body.style.overflow = 'auto';
+  }
+}, nav);
+
